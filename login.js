5302d562c5e13adc0b7e49c98d81047b8b060eca
Gaurav <gangulygaurav166@gmail.com>
2022-09-20 19:25:46 +0530

    fixed prime problem

--- login.js
+++ login.js
@@ -1,12 +1,20 @@
 
-let num=13 
-let count=0;
-    for(let i=1;i<=num;i++){
-        if(num%i==0){
-            count++
+
+function checkPrime(num){
+    let count=0;
+        for(let i=1;i<=num;i++){
+            if(num%i==0){
+                count++;
+            }
         }
+    if(count==2){
+        return true;
+    }else{
+        return false;
     }
-if(count==2){
+}
+
+if(checkPrime(13)){
     console.log("number is prime");
 }else{
     console.log("num is not prime");
